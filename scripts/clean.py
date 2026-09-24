4537e0cd18e8958bbc24686e518cd88c89df5cb0
chriisero <chris.vill@icloud.com>
2026-09-24 04:07:19 +0200

    fixed clean.py and clean-all.py

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import shutil
 
-root = Path(__file__).resolve().parent
+root = Path(__file__).resolve().parent.parent
 
 for path in root.rglob("__pycache__"):
     if path.is_dir():
